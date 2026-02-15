600b51a87552577537f9be8b04e986b4506a450c
Andre Santos <andre.celestino.santos@gmail.com>
2026-02-15 14:55:20 -0300

    first commit

---
+++ src/main/java/com/andre/orders_apis/OrdersApisApplication.java
@@ -0,0 +1,13 @@
+package com.andre.orders_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/orders_apis/OrdersApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.orders_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
